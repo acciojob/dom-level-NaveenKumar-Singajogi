279df795fbccd227d31026fa7d987b98b167f3b7
NaveenKumar-Singajogi <naveenjogi40@gmail.com>
2025-12-31 16:57:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const obj=document.getElementById("#level");
+let level=0;
+let parent=item;
+while(parent){
+	level+=1;
+	parent=parent.parentElement;
+}
+alert(`The level of the element is:"${level}`);
+
