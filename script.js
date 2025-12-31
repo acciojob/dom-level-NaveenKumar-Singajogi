843148a49bf8aa9d35cbacd6095eff344f87f2b0
NaveenKumar-Singajogi <naveenjogi40@gmail.com>
2025-12-31 17:03:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
-const obj=document.getElementById("#level");
+const obj=document.querySelector("#level");
 let level=0;
-let parent=item;
+let parent="item";
 while(parent){
 	level+=1;
 	parent=parent.parentElement;
